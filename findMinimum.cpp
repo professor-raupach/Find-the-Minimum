2f63043ee194862562fc5cafe44618a1c068061e
Paul Raupach <paul.raupach@csueastbay.edu>
2021-12-17 12:54:48 -0800

    Update findMinimum.cpp

--- findMinimum.cpp
+++ findMinimum.cpp
@@ -4,16 +4,17 @@
 int findMinimum(int collection[], int size) {
     // complete the function so that it returns the
     // minimum value from the collection array.
+    
     return 0;
 }
 
 
-int main() {
-    int list[] = {35, 63,22, 86, 37, 82, 99, 18, 109, 64};
-    int min = findMinimum(list, 10);
-    std::cout << "The minimum is " << min << std::endl;
-    return 0;
-}
+// int main() {
+//    int list[] = {35, 63,22, 86, 37, 82, 99, 18, 109, 64};
+//    int min = findMinimum(list, 10);
+//    std::cout << "The minimum is " << min << std::endl;
+//    return 0;
+//}
 
 
 #endif
